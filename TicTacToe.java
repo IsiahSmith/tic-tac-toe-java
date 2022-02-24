020682c3075ebf0cde7b950da6444de24d2041ca
Isiah Smith <isiahthomassmith@gmail.com>
2022-02-23 23:25:07 -0600

    switch cases cleaned up to allow cpu inputs

--- TicTacToe.java
+++ TicTacToe.java
@@ -19,35 +19,7 @@ public class TicTacToe {
 
         System.out.println(pos);
 
-        switch(pos) {
-            case 1:
-                gameBoard [0][0] = 'X';
-                break;
-            case 2:
-                gameBoard [0][2] = 'X';
-                break;
-            case 3:
-                gameBoard [0][4] = 'X';
-                break;
-            case 4:
-                gameBoard [2][0] = 'X';
-                break;
-            case 5:
-                gameBoard [2][2] = 'X';
-                break;
-            case 6:
-                gameBoard [2][4] = 'X';
-                break;
-            case 7:
-                gameBoard [4][0] = 'X';
-                break;
-            case 8:
-                gameBoard [4][2] = 'X';
-                break;
-            case 9:
-                gameBoard [4][4] = 'X';
-                break;
-        }
+        playerPlacement(gameBoard, pos, "player");
 
         printGameBoard(gameBoard);
 
@@ -60,6 +32,49 @@ public class TicTacToe {
                 System.out.print(c);
             }
             System.out.println();
+        }
+    }
+
+    public static void playerPlacement(char[][] gameBoard, int pos, String user) {
+
+        char symbol = ' ';
+
+        if(user.equals("player")) {
+            symbol = 'X';
+        } else if(user.equals("cpu")) {
+            symbol = 'O';
+        }
+
+        switch(pos) {
+            case 1:
+                gameBoard [0][0] = symbol;
+                break;
+            case 2:
+                gameBoard [0][2] = symbol;
+                break;
+            case 3:
+                gameBoard [0][4] = symbol;
+                break;
+            case 4:
+                gameBoard [2][0] = symbol;
+                break;
+            case 5:
+                gameBoard [2][2] = symbol;
+                break;
+            case 6:
+                gameBoard [2][4] = symbol;
+                break;
+            case 7:
+                gameBoard [4][0] = symbol;
+                break;
+            case 8:
+                gameBoard [4][2] = symbol;
+                break;
+            case 9:
+                gameBoard [4][4] = symbol;
+                break;
+            default:
+                break;
         }
     }
 
