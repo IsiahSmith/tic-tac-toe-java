1ec95bc7b2cdfd6a04702907d87ce48074f45c0c
Isiah Smith <isiahthomassmith@gmail.com>
2022-02-23 22:43:05 -0600

    gameBoard properly displaying in console

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,9 +2,19 @@ public class TicTacToe {
 
     public static void main(String[] args) {
         
-        char[][] gameBoard = {{' ', ' ', ' '}, 
-                              {' ', ' ', ' '}, 
-                              {' ', ' ', ' '}};
+        char[][] gameBoard = {
+            {' ', '|', ' ', '|', ' '}, 
+            {'-', '+', '-', '+', '-'}, 
+            {' ', '|', ' ', '|', ' '}, 
+            {'-', '+', '-', '+', '-'}, 
+            {' ', '|', ' ', '|', ' '}};
+
+        for(char[] row : gameBoard) {
+            for(char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
 
     }
 
